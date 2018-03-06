cd5bed56ab3556ce6d53a06846576b3d1944e710
Simon Vidanovic <dvvidanovic@lbl.gov>
2018-03-06 10:47:26 -0800

    Analytical solution for convection boundary conditions (test case).

--- tst/Analytical_ConvectionBC_Transient.unit.cxx
+++ tst/Analytical_ConvectionBC_Transient.unit.cxx
@@ -8,10 +8,11 @@ using namespace MoisThermFEM;
 /////////////////////////////////////////////////////////////////////////////////////
 /// Transient temperature boundary conditions vs Analytical solution
 ///
-/// This is test against analytical solution obtained from Carslaw-Jeager: page 97
+/// This is test against analytical solution obtained from Carslaw-Jeager: page 122
+/// NOTE: Carslaw-Jeager equation works only for specific coefficients (as used in example).
 /////////////////////////////////////////////////////////////////////////////////////
 
-class Analytical_TemperatureBC_Transient : public testing::Test {
+class Analytical_ConvectionBC_Transient : public testing::Test {
 
 protected:
 	void
@@ -26,7 +27,7 @@ protected:
 
 };
 
-TEST_F( Analytical_TemperatureBC_Transient, TestExample_1 ) {
+TEST_F( Analytical_ConvectionBC_Transient, TestExample_1 ) {
 	SCOPED_TRACE( "Begin Test: Example." );
 
 	// Enter nodes. Arguments are: node number, x-coordinate, y-coordinate, initial temperature
@@ -34,9 +35,10 @@ TEST_F( Analytical_TemperatureBC_Transient, TestExample_1 ) {
 	auto & materialPool = MaterialPool::Instance();
 
 	/// Create slab that is 10 cm long and have nodes at every 1 cm
-	std::vector< double > gridXCoordinates{ 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 };
+	std::vector< double > gridXCoordinates{ 0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
+																					0.1 };
 
-	const auto initialTemperature = 1.0;
+	const auto initialTemperature = 20.0;
 	const auto initialHumidity = 0.0;
 	const auto initialPressure = 101325.0;
 
@@ -52,10 +54,10 @@ TEST_F( Analytical_TemperatureBC_Transient, TestExample_1 ) {
 
 	auto & material = materialPool.createMaterial(
 			"Test Material",
-			1.0,    /// Density
+			2050,    /// Density
 			0.00,    /// Porosity
-			1.0,     /// Specific Heat Capacity (dry)
-			1.0,     /// Thermal Conductivity (dry)
+			850,     /// Specific Heat Capacity (dry)
+			1.8,     /// Thermal Conductivity (dry)
 			/// No need for liquid coefficients
 			15E-6,   /// Diffusion Resistance Factor
 			{ { 0, 0 } },  /// Liquid Transportation Coefficient
@@ -68,23 +70,22 @@ TEST_F( Analytical_TemperatureBC_Transient, TestExample_1 ) {
 	/// Create elements
 	for ( size_t i = 1; i <= ( nodePool.maxIndex() - 2 ) / 2; ++i ) {
 		auto node1 = nodePool.Instance().getNode( 2 * i - 1 );
-		auto node2 = nodePool.Instance().getNode( 2 * i + 1 );
+		auto node2 = nodePool.Instance().getNode( 2 * i );
 		auto node3 = nodePool.Instance().getNode( 2 * i + 2 );
-		auto node4 = nodePool.Instance().getNode( 2 * i );
+		auto node4 = nodePool.Instance().getNode( 2 * i + 1 );
 
 		domain.elementsCreator().createThermalElement( node1, node2, node3, node4, material );
 	}
 
 	// Create Boundary Conditions
-	const auto tAir = 0.0;
-	const auto hc = 1.0;
+	const auto tSurface = 0.0;
 
 	auto nodeBC1 = nodePool.Instance().getNode( 21 );
 	auto nodeBC2 = nodePool.Instance().getNode( 22 );
 
-	domain.boundariesCreator().createConvectionBC( nodeBC1, nodeBC2, hc, tAir );
+	domain.boundariesCreator().createTemperatureBC( nodeBC1, nodeBC2, tSurface );
 
-	const auto dTime = 0.001;
+	const auto dTime = 36;
 	const auto nSteps = 1000;
 
 	auto temperatures = NodePool::Instance().nodeProperties( Property::temperature );
@@ -96,23 +97,23 @@ TEST_F( Analytical_TemperatureBC_Transient, TestExample_1 ) {
 	}
 
 	std::vector< std::vector< double > > analyticalSolution = {
-			{ 0.99311, 0.95051, 0.72358 },
-			{ 0.95064, 0.87925, 0.64339 },
-			{ 0.89180, 0.81526, 0.58885 },
-			{ 0.83095, 0.75671, 0.54417 },
-			{ 0.77253, 0.70260, 0.50452 },
-			{ 0.71768, 0.65243, 0.46827 },
-			{ 0.66656, 0.60587, 0.43478 },
-			{ 0.61903, 0.56264, 0.40374 },
-			{ 0.57487, 0.52250, 0.37493 },
-			{ 0.53386, 0.48522, 0.34818 }
+			{ 10.171, 7.195, 0.000 },
+			{ 4.064,  2.874, 0.000 },
+			{ 1.623,  1.148, 0.000 },
+			{ 0.649,  0.459, 0.000 },
+			{ 0.259,  0.183, 0.000 },
+			{ 0.104,  0.073, 0.000 },
+			{ 0.041,  0.029, 0.000 },
+			{ 0.017,  0.012, 0.000 },
+			{ 0.007,  0.005, 0.000 },
+			{ 0.003,  0.002, 0.000 }
 	};
 
 	EXPECT_EQ( solution.size(), analyticalSolution.size() * 100 );
 
 	for ( auto i = 0u; i < analyticalSolution.size(); ++i ) {
 		for ( auto j = 0u; j < analyticalSolution[ i ].size(); ++j ) {
-			EXPECT_NEAR( analyticalSolution[ i ][ j ], solution[ 100 * i + 99 ][ j * 10 ], 0.002 );
+			EXPECT_NEAR( analyticalSolution[ i ][ j ], solution[ 100 * i + 99 ][ j * 10 ], 0.05 );
 		}
 	}
 }
