388ac9a143877c819b91f1761dffed0cf28bbf12
Simon Vidanovic <dvvidanovic@lbl.gov>
2018-05-22 13:55:24 -0700

    Eigen's SparceMatrix is not base for SquareMatrix.

--- src/SquareMatrix.hxx
+++ src/SquareMatrix.hxx
@@ -2,20 +2,23 @@
 
 #include <cstddef>
 #include <vector>
+
 #pragma warning(push,0)
 #include <Eigen/Core>
+#include <Eigen/SparseCore>
 #pragma warning(pop)
 
 namespace FenestrationCommon
 {
-	class CLinearSolver;
-
     // Works only with double
     class SquareMatrix
     {
-    	friend class CLinearSolver;
+		friend class CLinearSolver;
     public:
         explicit SquareMatrix(const std::size_t m_size);
+		// Constructor with triplets should be used for huge matrices. For example 100,000 x 100,000 will not fit
+		// into memory of vector<vector<double>>
+		explicit SquareMatrix(const std::size_t m_size, const std::vector<Eigen::Triplet<double>> & tripletList);
         explicit SquareMatrix(const std::initializer_list<std::vector<double>> & tInput);
         explicit SquareMatrix(const std::vector<std::vector<double>> & tInput);
         explicit SquareMatrix(const std::vector<std::vector<double>> && tInput);
@@ -24,6 +27,7 @@ namespace FenestrationCommon
         void setZeros();
         void setIdentity();
         void setDiagonal(const std::vector<double> & tInput);
+		SquareMatrix addDiagonal(const std::vector<double> & tInput) const;
 
         SquareMatrix inverse() const;
 
@@ -31,8 +35,9 @@ namespace FenestrationCommon
         double & operator()(const std::size_t i, const std::size_t j);
 
         SquareMatrix mmultRows(const std::vector<double> & tInput) const;
-        SquareMatrix addDiagonal(const std::vector<double> & tInput) const;
 
+		SquareMatrix operator*(const double value) const;
+		friend SquareMatrix operator*(const double value, const SquareMatrix & other);
         friend SquareMatrix operator*(const SquareMatrix & first, const SquareMatrix & second);
         SquareMatrix & operator*=(const SquareMatrix & other);
         SquareMatrix & operator+(const SquareMatrix & other);
@@ -41,16 +46,15 @@ namespace FenestrationCommon
         SquareMatrix & operator-=(const SquareMatrix & other);
 
         std::vector<double> operator*(const std::vector<double> & tVec) const;
-		friend std::vector<double> operator*(const std::vector<double> & first, const SquareMatrix & second);
-		friend SquareMatrix operator*(const double value, const SquareMatrix & matrix);
-		friend SquareMatrix operator*(const SquareMatrix & matrix, const double value);
-
+        friend std::vector<double> operator*(const std::vector<double> & first,
+                                             const SquareMatrix & second);
 
     private:
-        explicit SquareMatrix(Eigen::MatrixXd && tMatrix);
-		explicit SquareMatrix(Eigen::MatrixXd & tMatrix);
+        // explicit SquareMatrix(Eigen::MatrixXd && tMatrix);
+        explicit SquareMatrix(Eigen::SparseMatrix<double> && tMatrix);
         std::size_t m_size;
-        Eigen::MatrixXd m_Matrix;
+        Eigen::SparseMatrix<double> m_Matrix;
+        // Eigen::MatrixXd m_Matrix;
     };
 
 }   // namespace FenestrationCommon
