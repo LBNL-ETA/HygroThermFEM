5b11d9bb0c6c60d4958846832515565b703db5cb
Simon Vidanovic <dvvidanovic@lbl.gov>
2019-05-30 13:17:45 -0700

    simulation properties are now extended for calculation properties.

--- src/SimulationProperties.cxx
+++ src/SimulationProperties.cxx
@@ -11,8 +11,32 @@ namespace HygroThermFEM
     SimulationProperties::SimulationProperties() :
         m_RelaxationParameter(defaultProperties.relaxationParameter),
         m_ErrorTolerance(defaultProperties.errorTolerance),
-        m_MaxNumberOfIterations(defaultProperties.maxIterations)
+        m_MaxNumberOfIterations(defaultProperties.maxIterations),
+        m_ExcludeWaterLiquidTransportation(defaultProperties.excludeWaterLiquidTransportation),
+        m_ExcludeHeatOfEvaporation(defaultProperties.excludeHeatOfEvaporation),
+        m_ExcludeCapillaryConduction(defaultProperties.excludeCapillaryConduction),
+        m_ExcludeVaporDiffusionConduction(defaultProperties.excludeVaporDiffusionConduction)
     {}
+
+    bool SimulationProperties::excludeWaterLiquidTransportation() const
+    {
+        return m_ExcludeWaterLiquidTransportation;
+    }
+
+    bool SimulationProperties::excludeHeatOfEvaporation() const
+    {
+        return m_ExcludeHeatOfEvaporation;
+    }
+
+    bool SimulationProperties::excludeCapillaryConduction() const
+    {
+        return m_ExcludeCapillaryConduction;
+    }
+
+    bool SimulationProperties::excludeVaporDiffusionConduction() const
+    {
+        return m_ExcludeVaporDiffusionConduction;
+    }
 
     void SimulationProperties::setIterationParameters(const double t_RelaxationParameter,
                                                       const double t_ErrorTolerance,
@@ -21,6 +45,38 @@ namespace HygroThermFEM
         m_RelaxationParameter = t_RelaxationParameter;
         m_ErrorTolerance = t_ErrorTolerance;
         m_MaxNumberOfIterations = t_MaxNumberOfIterations;
+    }
+
+    void SimulationProperties::resetIterationParameters()
+    {
+        m_RelaxationParameter = defaultProperties.relaxationParameter;
+        m_ErrorTolerance = defaultProperties.errorTolerance;
+        m_MaxNumberOfIterations = defaultProperties.maxIterations;
+    }
+
+    void SimulationProperties::setCalculationParameters(bool excludeWaterLiquidTransportation,
+                                                        bool excludeHeatOfEvaporation,
+                                                        bool excludeCapillaryConduction,
+                                                        bool excludeVaporDiffusionConduction)
+    {
+        m_ExcludeWaterLiquidTransportation = excludeWaterLiquidTransportation;
+        m_ExcludeHeatOfEvaporation = excludeHeatOfEvaporation;
+        m_ExcludeCapillaryConduction = excludeCapillaryConduction;
+        m_ExcludeVaporDiffusionConduction = excludeVaporDiffusionConduction;
+    }
+
+    void SimulationProperties::resetCalculationParameters()
+    {
+        m_ExcludeWaterLiquidTransportation = defaultProperties.excludeWaterLiquidTransportation;
+        m_ExcludeHeatOfEvaporation = defaultProperties.excludeHeatOfEvaporation;
+        m_ExcludeCapillaryConduction = defaultProperties.excludeCapillaryConduction;
+        m_ExcludeVaporDiffusionConduction = defaultProperties.excludeVaporDiffusionConduction;
+    }
+
+    void SimulationProperties::reset()
+    {
+        resetIterationParameters();
+        resetCalculationParameters();
     }
 
     double SimulationProperties::relaxationParamter() const
--- src/SimulationProperties.hxx
+++ src/SimulationProperties.hxx
@@ -9,27 +9,89 @@ namespace HygroThermFEM
     public:
         static SimulationProperties & Instance();
 
-        void setIterationParameters(double t_RelaxationParameter,
-                                    double t_ErrorTolerance,
-                                    std::size_t t_MaxNumberOfIterations);
+        //! \brief Sets iteration parameters for use in every Finite Element Method domain defined
+        //! in this engine.
+        //!
+        //! @param relaxationParameter Number that is > 0 and <=1 that will be used in
+        //! iterations to recalculate state variable for start in the next iteration.
+        //! @param errorTolerance Tolerance for which solution will be acceptable.
+        //! @param maxNumberOfIterations Number of iterations for which solver will attempt to reach
+        //! solution with given parameters.
+        void setIterationParameters(double relaxationParameter,
+                                    double errorTolerance,
+                                    std::size_t maxNumberOfIterations);
 
+        //! \brief Resets iteration parameters back to default.
+        void resetIterationParameters();
+
+        //! \brief Sets simulation parameters for use in thermal and moisture domains. It defines
+        //! what parts of equations will be excluded from finite element model calculations.
+        //!
+        //! @param excludeWaterLiquidTransportation If set to true, it will exclude water liquid
+        //! transportation from mass transfer equation.
+        //! @param excludeHeatOfEvaporation If set to true, it will exclude heat of evaporation
+        //! calculation from heat transfer equation.
+        //! @param excludeCapillaryConduction If set to true, it will exclude capillary conduction
+        //! calculation from heat transfer equation.
+        //! @param excludeVaporDiffusionConduction If set to true, it will exclude water vapor
+        //! diffusion conduction from heat transfer equation.
+        void setCalculationParameters(bool excludeWaterLiquidTransportation,
+                                      bool excludeHeatOfEvaporation,
+                                      bool excludeCapillaryConduction,
+                                      bool excludeVaporDiffusionConduction);
+
+        //! \brief Resets calculation parameters back to default.
+        void resetCalculationParameters();
+
+        //! \brief Resets all parameters back to default.
+        void reset();
+
+        //! \brief Returns current relaxation parameter value.
         double relaxationParamter() const;
+
+        //! \brief Returns current error tolerance.
         double errorTolerance() const;
+
+        //! \brief Returns current maximum allowed number of iterations.
         std::size_t maxNumberOfIterations() const;
+
+        //! \brief Defines if liquid transportation should be taken into account during mass
+        //! transfer calculations.
+        bool excludeWaterLiquidTransportation() const;
+
+        //! \brief Defines if heat of evaporation should be taken into account during heat transfer
+        //! calculations.
+        bool excludeHeatOfEvaporation() const;
+
+        //! \brief Defines if capillary conduction should be taken into account during heat transfer
+        //! calculations.
+        bool excludeCapillaryConduction() const;
+
+        //! \brief Defines if vapor diffusion conduction should be taken into account during heat
+        //! transfer calculations.
+        bool excludeVaporDiffusionConduction() const;
 
     private:
         SimulationProperties();
-        ~SimulationProperties() = default;
 
+        //! \brief Keeps default parameters in single place
         struct DefaultProperties
         {
             double relaxationParameter{1};
             double errorTolerance{1e-5};
-            double maxIterations{50};
+            size_t maxIterations{50};
+            bool excludeWaterLiquidTransportation{false};
+            bool excludeHeatOfEvaporation{false};
+            bool excludeCapillaryConduction{false};
+            bool excludeVaporDiffusionConduction{false};
         } defaultProperties;
 
         double m_RelaxationParameter;
         double m_ErrorTolerance;
         size_t m_MaxNumberOfIterations;
+        bool m_ExcludeWaterLiquidTransportation;
+        bool m_ExcludeHeatOfEvaporation;
+        bool m_ExcludeCapillaryConduction;
+        bool m_ExcludeVaporDiffusionConduction;
     };
 }   // namespace HygroThermFEM
