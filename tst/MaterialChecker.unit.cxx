a329873725ce946ab8da51817dfecb6df5f18e10
vidanovic <DVVidanovic@lbl.gov>
2020-01-06 16:33:20 -0800

    Test case when only thermal simulation is on.

--- tst/MaterialChecker.unit.cxx
+++ tst/MaterialChecker.unit.cxx
@@ -63,4 +63,52 @@ TEST_F(TestMaterialChecker, TestExample_1)
     EXPECT_EQ(matCheckSteadyState[0].LiquidTransportationSuction, true);
     EXPECT_EQ(matCheckSteadyState[0].LiquidTransportationRedistribution, false);
     EXPECT_EQ(matCheckSteadyState[0].ThermalConductivityMoistureAndTemperatureDependent, false);
+
+    MaterialPool::Instance().clear();
+}
+
+TEST_F(TestMaterialChecker, TestExample_2)
+{
+    SCOPED_TRACE("Test for material missing properties in case of only thermal simulation.");
+
+    // Note that material is incomplete
+    MaterialPool::Instance().createSolidMaterial("Test material");
+
+    const bool SimulateThermal{true};
+    const bool SimulateMoisture{false};
+    HygroThermFEM::MultiDomain domain{SimulateThermal, SimulateMoisture};
+
+    // Check for transient simulation
+    const auto matCheckTransient = domain.checkMaterialsForTransientSimulation();
+    EXPECT_EQ(matCheckTransient.size(), 1);
+
+    // Series of test means following. Material property is missing for given simulation properties
+    // and therefore it is set to true. If material property is not needed for given simulation
+    // properties then it is set to false.
+    EXPECT_EQ(matCheckTransient[0].Density, true);
+    EXPECT_EQ(matCheckTransient[0].Emissivity, true);
+    EXPECT_EQ(matCheckTransient[0].Porosity, false);
+    EXPECT_EQ(matCheckTransient[0].SpecificHeatCapacityDry, true);
+    EXPECT_EQ(matCheckTransient[0].ThermalConductivityDry, true);
+    EXPECT_EQ(matCheckTransient[0].WaterVaporDiffusionResistanceFactor, true);
+    EXPECT_EQ(matCheckTransient[0].MoistureStorageFunction, true);
+    EXPECT_EQ(matCheckTransient[0].LiquidTransportationSuction, true);
+    EXPECT_EQ(matCheckTransient[0].LiquidTransportationRedistribution, false);
+    EXPECT_EQ(matCheckTransient[0].ThermalConductivityMoistureAndTemperatureDependent, false);
+
+    // Check for steady-state simulation
+    const auto matCheckSteadyState = domain.checkMaterialsForSteadyStateSimulation();
+    EXPECT_EQ(matCheckSteadyState.size(), 1);
+    EXPECT_EQ(matCheckSteadyState[0].Density, false);
+    EXPECT_EQ(matCheckSteadyState[0].Emissivity, true);
+    EXPECT_EQ(matCheckSteadyState[0].Porosity, false);
+    EXPECT_EQ(matCheckSteadyState[0].SpecificHeatCapacityDry, false);
+    EXPECT_EQ(matCheckSteadyState[0].ThermalConductivityDry, true);
+    EXPECT_EQ(matCheckSteadyState[0].WaterVaporDiffusionResistanceFactor, true);
+    EXPECT_EQ(matCheckSteadyState[0].MoistureStorageFunction, true);
+    EXPECT_EQ(matCheckSteadyState[0].LiquidTransportationSuction, true);
+    EXPECT_EQ(matCheckSteadyState[0].LiquidTransportationRedistribution, false);
+    EXPECT_EQ(matCheckSteadyState[0].ThermalConductivityMoistureAndTemperatureDependent, false);
+
+    MaterialPool::Instance().clear();
 }
