282affc3e48795342a04c8846b790f5659aa17ac
Simon Vidanovic <dvvidanovic@lbl.gov>
2020-01-27 09:31:04 -0800

    Added two repeatability tests.

--- tst/MultiDomain_2D_1.unit.cxx
+++ tst/MultiDomain_2D_1.unit.cxx
@@ -183,3 +183,164 @@ TEST_F(MultiDomain_2D_1, TestExample_1)
         }
     }
 }
+
+TEST_F(MultiDomain_2D_1, TestExample_1_Repeat)
+{
+    SCOPED_TRACE("Begin Test: Repeatability test.");
+
+    NodePool::Instance().clear();
+    MaterialPool::Instance().clear();
+
+    // Enter nodes. Arguments are: node number, x-coordinate, y-coordinate
+
+    std::vector<double> gridXCoordinates{0, 0.05, 0.1};
+
+    const double initialTemperature = 20;
+    const double initialMoistureContent = 0.0;
+    const double initialPressure = 101325;
+    const auto liquidPercent = 1.0;
+
+    auto state = State(initialTemperature, initialMoistureContent, initialPressure, liquidPercent);
+    size_t nodeIndex = 0;
+    auto T = 0.0;
+    auto deltaT = 10.0;
+    auto H = 0.0;
+    auto deltaH = 0.1;
+    for(auto val : gridXCoordinates)
+    {
+        ++nodeIndex;
+        NodePool::Instance().createNode(
+                nodeIndex,
+                val,
+                0.00,
+                State(initialTemperature + T, initialMoistureContent + H, initialPressure, 0));
+        ++nodeIndex;
+        NodePool::Instance().createNode(
+                nodeIndex,
+                val,
+                0.05,
+                State(initialTemperature + T, initialMoistureContent + H, initialPressure, 0));
+        T += deltaT;
+        H += deltaH;
+    }
+
+    // Material Properties (Cottaer Sandstone)
+    const double thermalConductivityDry{1.8};
+    const double density{2050.0};
+    const double porosity{0.22};
+    const double specificHeatCapacityDry{850.0};
+    const double diffusionResistanceFactor{15.0};
+    const std::vector<FenestrationCommon::point> thermalConductivityMoistureDependent = {
+            {0.0, 1.8}, {180, 1.8}};
+    const double thermalConductivityMeasuredAtTemperature{0};
+    const std::vector<FenestrationCommon::point> thermalConductivityTemperatureDependent = {
+            {0.0, 1.8}, {1, 1.8}};
+    const double thermalConductivityMeasuredAtHumidity{0};
+    const std::vector<FenestrationCommon::point> liquidTransportationCurve = {{0, 0},
+                                                                              {27, 1E-8},
+                                                                              {45, 1.1E-8},
+                                                                              {90, 2E-8},
+                                                                              {126, 3.5E-8},
+                                                                              {144, 5E-8},
+                                                                              {162, 1E-7},
+                                                                              {171, 2E-7},
+                                                                              {180, 7E-7}};
+
+    const std::vector<FenestrationCommon::point> moistureStorageFunction = {{0, 0},
+                                                                            {0.5, 5.3},
+                                                                            {0.65, 8.4},
+                                                                            {0.8, 12},
+                                                                            {0.93, 17},
+                                                                            {0.95, 25},
+                                                                            {0.99, 63},
+                                                                            {0.995, 83},
+                                                                            {0.999, 120},
+                                                                            {1, 180}};
+
+    auto & material =
+            MaterialPool::Instance().createSolidMaterial("Cottaer Sandstone",
+                                                         thermalConductivityDry,
+                                                         density,
+                                                         porosity,
+                                                         specificHeatCapacityDry,
+                                                         diffusionResistanceFactor,
+                                                         thermalConductivityMoistureDependent,
+                                                         thermalConductivityMeasuredAtTemperature,
+                                                         thermalConductivityTemperatureDependent,
+                                                         thermalConductivityMeasuredAtHumidity,
+                                                         liquidTransportationCurve,
+                                                         moistureStorageFunction);
+
+    HygroThermFEM::MultiDomain domain;
+
+    /// Create elements
+    for(size_t i = 1; i <= (NodePool::Instance().maxIndex() - 2) / 2; ++i)
+    {
+        const auto node1 = 2u * i + 1u;
+        const auto node2 = 2u * i + 2u;
+        const auto node3 = 2u * i;
+        const auto node4 = 2u * i - 1u;
+        domain.createElement(node1, node2, node3, node4, material.name());
+    }
+
+    const auto dTime = 360;
+    const auto nSteps = 10;
+
+    auto temperatures = NodePool::Instance().properties(HygroThermFEM::Variable::temperature);
+    auto humidities = NodePool::Instance().properties(HygroThermFEM::Variable::humidity);
+    std::vector<std::vector<double>> temperatureSolution;
+    std::vector<std::vector<double>> waterContentSolution;
+
+    for(auto i = 0; i < nSteps; ++i)
+    {
+        auto aSolution = domain.transient(temperatures, humidities, dTime);
+        temperatureSolution.push_back(aSolution.temperature);
+        waterContentSolution.push_back(aSolution.waterContent);
+        temperatures = aSolution.temperature;
+        humidities = aSolution.humidity;
+    }
+
+    std::vector<std::vector<double>> correctWaterContentSolution{
+            {0.000944, 0.000944, 1.060053, 1.060053, 2.117104, 2.117104},
+            {0.001987, 0.001987, 1.060083, 1.060083, 2.114588, 2.114588},
+            {0.003113, 0.003113, 1.060099, 1.060099, 2.112348, 2.112348},
+            {0.004305, 0.004305, 1.060107, 1.060107, 2.110308, 2.110308},
+            {0.005551, 0.005551, 1.060109, 1.060109, 2.108417, 2.108417},
+            {0.006839, 0.006839, 1.060109, 1.060109, 2.106638, 2.106638},
+            {0.008159, 0.008159, 1.060107, 1.060107, 2.104943, 2.104943},
+            {0.009505, 0.009505, 1.060103, 1.060103, 2.103312, 2.103312},
+            {0.01087, 0.01087, 1.060099, 1.060099, 2.101731, 2.101731},
+            {0.01225, 0.01225, 1.060094, 1.060094, 2.100187, 2.100187}};
+
+    EXPECT_EQ(waterContentSolution.size(), correctWaterContentSolution.size());
+
+    for(auto i = 0u; i < correctWaterContentSolution.size(); ++i)
+    {
+        for(auto j = 0u; j < correctWaterContentSolution[i].size(); ++j)
+        {
+            EXPECT_NEAR(correctWaterContentSolution[i][j], waterContentSolution[i][j], 1e-6);
+        }
+    }
+
+    std::vector<std::vector<double>> correctTemperatureSolution{
+            {22.291791, 22.291791, 30.001060, 30.001060, 37.718922, 37.718922},
+            {24.058697, 24.058697, 30.002681, 30.002681, 35.958658, 35.958658},
+            {25.421047, 25.421047, 30.004433, 30.004433, 34.600417, 34.600417},
+            {26.471542, 26.471542, 30.006097, 30.006097, 33.552457, 33.552457},
+            {27.281613, 27.281613, 30.007575, 30.007575, 32.743944, 32.743944},
+            {27.906314, 27.906314, 30.008837, 30.008837, 32.120196, 32.120196},
+            {28.388083, 28.388083, 30.009887, 30.009887, 31.639007, 31.639007},
+            {28.759632, 28.759632, 30.010743, 30.010743, 31.267808, 31.267808},
+            {29.046186, 29.046186, 30.011433, 30.011433, 30.981463, 30.981463},
+            {29.267192, 29.267192, 30.011984, 30.011984, 30.760580, 30.760580}};
+
+    EXPECT_EQ(temperatureSolution.size(), correctTemperatureSolution.size());
+
+    for(auto i = 0u; i < correctTemperatureSolution.size(); ++i)
+    {
+        for(auto j = 0u; j < correctTemperatureSolution[i].size(); ++j)
+        {
+            EXPECT_NEAR(correctTemperatureSolution[i][j], temperatureSolution[i][j], 1e-6);
+        }
+    }
+}
--- tst/MultiDomain_2D_2.unit.cxx
+++ tst/MultiDomain_2D_2.unit.cxx
@@ -172,3 +172,158 @@ TEST_F(MultiDomain_2D_2, TestExample_1)
         }
     }
 }
+
+TEST_F(MultiDomain_2D_2, TestExample_1_Repeat)
+{
+    SCOPED_TRACE("Begin Test: Repeatability test.");
+
+    NodePool::Instance().clear();
+    MaterialPool::Instance().clear();
+
+    std::vector<double> gridXCoordinates{0, 0.05, 0.1};
+
+    const double initialTemperature = 0.0;
+    const double initialMoistureContent = 0.0;
+    const double initialPressure = 101325;
+    const auto liquidPercent = 1.0;
+
+    auto state = HygroThermFEM::State(
+            initialTemperature, initialMoistureContent, initialPressure, liquidPercent);
+    size_t nodeIndex = 0;
+    for(auto val : gridXCoordinates)
+    {
+        ++nodeIndex;
+        NodePool::Instance().createNode(nodeIndex, val, 0.00, state);
+        ++nodeIndex;
+        NodePool::Instance().createNode(nodeIndex, val, 0.05, state);
+    }
+
+    // Material Properties (Cottaer Sandstone)
+    const double thermalConductivityDry{1.8};
+    const double density{2050.0};
+    const double porosity{0.22};
+    const double specificHeatCapacityDry{850.0};
+    const double diffusionResistanceFactor{15.0};
+    const std::vector<FenestrationCommon::point> thermalConductivityMoistureDependent = {
+            {0.0, 1.8}, {180, 1.8}};
+    const double thermalConductivityMeasuredAtTemperature{0};
+    const std::vector<FenestrationCommon::point> thermalConductivityTemperatureDependent = {
+            {0.0, 1.8}, {1, 1.8}};
+    const double thermalConductivityMeasuredAtHumidity{0};
+    const std::vector<FenestrationCommon::point> liquidTransportationCurve = {{0, 0},
+                                                                              {27, 1E-8},
+                                                                              {45, 1.1E-8},
+                                                                              {90, 2E-8},
+                                                                              {126, 3.5E-8},
+                                                                              {144, 5E-8},
+                                                                              {162, 1E-7},
+                                                                              {171, 2E-7},
+                                                                              {180, 7E-7}};
+
+    const std::vector<FenestrationCommon::point> moistureStorageFunction = {{0, 0},
+                                                                            {0.5, 5.3},
+                                                                            {0.65, 8.4},
+                                                                            {0.8, 12},
+                                                                            {0.93, 17},
+                                                                            {0.95, 25},
+                                                                            {0.99, 63},
+                                                                            {0.995, 83},
+                                                                            {0.999, 120},
+                                                                            {1, 180}};
+
+    auto & material =
+            MaterialPool::Instance().createSolidMaterial("Cottaer Sandstone",
+                                                         thermalConductivityDry,
+                                                         density,
+                                                         porosity,
+                                                         specificHeatCapacityDry,
+                                                         diffusionResistanceFactor,
+                                                         thermalConductivityMoistureDependent,
+                                                         thermalConductivityMeasuredAtTemperature,
+                                                         thermalConductivityTemperatureDependent,
+                                                         thermalConductivityMeasuredAtHumidity,
+                                                         liquidTransportationCurve,
+                                                         moistureStorageFunction);
+
+    HygroThermFEM::MultiDomain domain;
+
+    /// Create elements
+    for(size_t i = 1; i <= (NodePool::Instance().maxIndex() - 2) / 2; ++i)
+    {
+        const auto node1 = 2u * i + 1u;
+        const auto node2 = 2u * i + 2u;
+        const auto node3 = 2u * i;
+        const auto node4 = 2u * i - 1u;
+        domain.createElement(node1, node2, node3, node4, material.name());
+    }
+
+    /// Create Boundary Conditions
+    const auto hc = 1.0;
+    const auto airTemperature = 20.0;
+    const auto humidity = 0.6;
+
+    const HygroThermFEM::FixedBCHCCoefficients bcCoeff{airTemperature, hc, humidity};
+
+    domain.createMoistureBCFixedHc(1, 2, bcCoeff);
+
+    const auto dTime = 3600;
+    const auto nSteps = 10;
+
+    auto temperatures = NodePool::Instance().properties(HygroThermFEM::Variable::temperature);
+    auto humidities = NodePool::Instance().properties(HygroThermFEM::Variable::humidity);
+    std::vector<std::vector<double>> temperatureSolution;
+    std::vector<std::vector<double>> waterContentSolution;
+
+    for(auto i = 0; i < nSteps; ++i)
+    {
+        auto aSolution = domain.transient(temperatures, humidities, dTime);
+        temperatureSolution.push_back(aSolution.temperature);
+        waterContentSolution.push_back(aSolution.waterContent);
+        temperatures = aSolution.temperature;
+        humidities = aSolution.humidity;
+    }
+
+    std::vector<std::vector<double>> correctWaterContentSolution{
+            {1.089281, 1.089281, 0.001245, 0.001245, 2e-06, 2e-06},
+            {2.11223, 2.11223, 0.00378, 0.00378, 1.1e-05, 1.1e-05},
+            {3.000406, 3.000406, 0.007521, 0.007521, 3e-05, 3e-05},
+            {3.72435, 3.72435, 0.012294, 0.012294, 6.2e-05, 6.2e-05},
+            {4.336618, 4.336618, 0.018007, 0.018007, 0.00011, 0.00011},
+            {4.866186, 4.866186, 0.024596, 0.024596, 0.00018, 0.00018},
+            {5.36041, 5.36041, 0.032008, 0.032008, 0.000273, 0.000273},
+            {6.163777, 6.163777, 0.040202, 0.040202, 0.000395, 0.000395},
+            {6.88133, 6.88133, 0.049141, 0.049141, 0.000549, 0.000549},
+            {7.531928, 7.531928, 0.058814, 0.058814, 0.00074, 0.00074}};
+
+    EXPECT_EQ(waterContentSolution.size(), correctWaterContentSolution.size());
+
+    for(auto i = 0u; i < correctWaterContentSolution.size(); ++i)
+    {
+        for(auto j = 0u; j < correctWaterContentSolution[i].size(); ++j)
+        {
+            EXPECT_NEAR(correctWaterContentSolution[i][j], waterContentSolution[i][j], 1e-6);
+        }
+    }
+
+    std::vector<std::vector<double>> correctTemperatureSolution{
+            {1.278961, 1.278961, 0.664541, 0.664541, 0.497362, 0.497362},
+            {2.103072, 2.103072, 1.389887, 1.389887, 1.165353, 1.165353},
+            {2.799476, 2.799476, 2.092455, 2.092455, 1.859221, 1.859221},
+            {3.443158, 3.443158, 2.763085, 2.763085, 2.535696, 2.535696},
+            {4.052248, 4.052248, 3.402328, 3.402328, 3.184303, 3.184303},
+            {4.631945, 4.631945, 4.011701, 4.011701, 3.803543, 3.803543},
+            {5.184333, 5.184333, 4.592612, 4.592612, 4.394094, 4.394094},
+            {5.710387, 5.710387, 5.146121, 5.146121, 4.956918, 4.956918},
+            {6.211405, 6.211405, 5.673432, 5.673432, 5.493160, 5.493160},
+            {6.688342, 6.688342, 6.175609, 6.175609, 6.003903, 6.003903}};
+
+    EXPECT_EQ(temperatureSolution.size(), correctTemperatureSolution.size());
+
+    for(auto i = 0u; i < correctTemperatureSolution.size(); ++i)
+    {
+        for(auto j = 0u; j < correctTemperatureSolution[i].size(); ++j)
+        {
+            EXPECT_NEAR(correctTemperatureSolution[i][j], temperatureSolution[i][j], 1e-6);
+        }
+    }
+}
