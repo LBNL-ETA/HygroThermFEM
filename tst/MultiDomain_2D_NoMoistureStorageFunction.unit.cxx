51a9aad57734912c005439aba732c36a04f55887
Simon Vidanovic <dvvidanovic@lbl.gov>
2020-03-06 12:49:16 -0800

    Zero moisture storage function is now possible.

---
+++ tst/MultiDomain_2D_NoMoistureStorageFunction.unit.cxx
@@ -0,0 +1,155 @@
+#include <memory>
+#include <gtest/gtest.h>
+
+#include "HygroThermFEM2D.hxx"
+
+using HygroThermFEM::NodePool;
+using HygroThermFEM::MaterialPool;
+using HygroThermFEM::SimulationProperties;
+
+class MultiDomain_2D_NoMoistureStorageFunction : public testing::Test
+{
+protected:
+    void SetUp() override
+    {}
+
+    void TearDown() override
+    {
+        NodePool::Instance().clear();
+        MaterialPool::Instance().clear();
+        SimulationProperties::Instance().reset();
+    }
+};
+
+TEST_F(MultiDomain_2D_NoMoistureStorageFunction, TestExample_1)
+{
+    const auto excludeWaterLiquidTransportation{false};
+    const auto excludeHeatOfEvaporation{false};
+    const auto excludeCapillaryConduction{false};
+    const auto excludeVaporDiffusionConduction{false};
+    const auto thermalConductivityMoistureAndTemperatureDependent{false};
+
+    SimulationProperties::Instance().setCalculationParameters(
+      excludeWaterLiquidTransportation,
+      excludeHeatOfEvaporation,
+      excludeCapillaryConduction,
+      excludeVaporDiffusionConduction,
+      thermalConductivityMoistureAndTemperatureDependent);
+
+    std::vector<double> gridXCoordinates{0, 0.05, 0.1};
+
+    const auto initialTemperature{0.0};
+    const auto initialMoistureContent{0.2};
+    const auto initialPressure{101325};
+
+    auto state = HygroThermFEM::State(initialTemperature, initialMoistureContent, initialPressure);
+    size_t nodeIndex = 0;
+    for(auto val : gridXCoordinates)
+    {
+        ++nodeIndex;
+        NodePool::Instance().createNode(nodeIndex, val, 0.00, state);
+        ++nodeIndex;
+        NodePool::Instance().createNode(nodeIndex, val, 0.05, state);
+    }
+
+    // Material Properties to represent example without moisture storage function and liquid
+    // transportation
+    const auto thermalConductivityDry{1.8};
+    const auto density{2050.0};
+    const auto porosity{0.22};
+    const auto specificHeatCapacityDry{850.0};
+    const auto diffusionResistanceFactor{15.0};
+    const std::vector<FenestrationCommon::point> thermalConductivityMoistureDependent = {
+      {0.0, 1.8}, {180, 1.8}};
+    const double thermalConductivityMeasuredAtTemperature{0};
+    const std::vector<FenestrationCommon::point> thermalConductivityTemperatureDependent = {
+      {0.0, 1.8}, {1, 1.8}};
+    const double thermalConductivityMeasuredAtHumidity{0};
+    const std::vector<FenestrationCommon::point> liquidTransportationCurve = {{0, 0}};
+
+    const std::vector<FenestrationCommon::point> moistureStorageFunction = {{0, 0}};
+
+    auto & material =
+      MaterialPool::Instance().createSolidMaterial("No liqud or vapor transport curves",
+                                                   thermalConductivityDry,
+                                                   density,
+                                                   porosity,
+                                                   specificHeatCapacityDry,
+                                                   diffusionResistanceFactor,
+                                                   thermalConductivityMoistureDependent,
+                                                   thermalConductivityMeasuredAtTemperature,
+                                                   thermalConductivityTemperatureDependent,
+                                                   thermalConductivityMeasuredAtHumidity,
+                                                   liquidTransportationCurve,
+                                                   moistureStorageFunction);
+
+    HygroThermFEM::MultiDomain domain;
+
+    /// Create elements
+    for(size_t i = 1; i <= (NodePool::Instance().maxIndex() - 2) / 2; ++i)
+    {
+        const auto node1 = 2u * i + 1u;
+        const auto node2 = 2u * i + 2u;
+        const auto node3 = 2u * i;
+        const auto node4 = 2u * i - 1u;
+        domain.createElement(node1, node2, node3, node4, material.name());
+    }
+
+    /// Create Boundary Conditions
+    const auto hc = 5.0;
+    const auto airTemperature = 10.0;
+    const auto humidity = 0.0;
+
+    const HygroThermFEM::FixedBCHCCoefficients bcCoeff{airTemperature, hc, humidity};
+
+    domain.createMoistureBCFixedHc(1, 2, bcCoeff);
+    domain.createMoistureBCFixedHc(5, 6, bcCoeff);
+
+    const auto dTime = 3600;
+    const auto nSteps = 4;
+
+    auto temperatures = NodePool::Instance().properties(HygroThermFEM::Variable::temperature);
+    auto humidities = NodePool::Instance().properties(HygroThermFEM::Variable::humidity);
+    std::vector<std::vector<double>> temperatureSolution;
+    std::vector<std::vector<double>> waterContentSolution;
+
+    for(auto i = 0; i < nSteps; ++i)
+    {
+        auto aSolution = domain.transient(temperatures, humidities, dTime);
+        temperatureSolution.push_back(aSolution.temperature);
+        waterContentSolution.push_back(aSolution.waterContent);
+        temperatures = aSolution.temperature;
+        humidities = aSolution.humidity;
+    }
+
+    std::vector<std::vector<double>> correctWaterContentSolution{{0, 0, 0, 0, 0, 0},
+                                                                 {0, 0, 0, 0, 0, 0},
+                                                                 {0, 0, 0, 0, 0, 0},
+                                                                 {0, 0, 0, 0, 0, 0}};
+
+    EXPECT_EQ(waterContentSolution.size(), correctWaterContentSolution.size());
+
+    for(auto i = 0u; i < waterContentSolution.size(); ++i)
+    {
+        for(auto j = 0u; j < waterContentSolution[i].size(); ++j)
+        {
+            EXPECT_NEAR(correctWaterContentSolution[i][j], waterContentSolution[i][j], 1e-6);
+        }
+    }
+
+    std::vector<std::vector<double>> correctTemperatureSolution{
+      {1.911518, 1.911518, 1.430638, 1.430638, 1.911518, 1.911518},
+      {3.291149, 3.291149, 2.823100, 2.823100, 3.291149, 3.291149},
+      {4.411504, 4.411504, 4.011909, 4.011909, 4.411504, 4.411504},
+      {5.341402, 5.341402, 5.006941, 5.006941, 5.341402, 5.341402}};
+
+    EXPECT_EQ(temperatureSolution.size(), correctTemperatureSolution.size());
+
+    for(auto i = 0u; i < temperatureSolution.size(); ++i)
+    {
+        for(auto j = 0u; j < temperatureSolution[i].size(); ++j)
+        {
+            EXPECT_NEAR(correctTemperatureSolution[i][j], temperatureSolution[i][j], 1e-6);
+        }
+    }
+}
